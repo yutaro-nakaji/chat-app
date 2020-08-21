fdb260e79995de6dbc8ff11b8e10bf43789ea90c
yutaro-nakaji <quadcore.4412gmail.com>
2020-08-21 15:28:34 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
 
-  def index
-     @message = Message.new
-     @room = Room.find(params[:room_id])
-     @messages = @room.messages.includes(:user)
-   end
+    def index
+      @message = Message.new
+      @room = Room.find(params[:room_id])
+      @messages = @room.messages.includes(:user)
+    end
  
    def create
      @room = Room.find(params[:room_id])
@@ -22,4 +22,4 @@ class MessagesController < ApplicationController
    def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
- end
+end
